a9c74c69aed6a5c25e116c694dac0d3aaaeac780
NadyaGus <ftotkexit@gmail.com>
2023-09-15 17:21:41 +0300

    init: start Brackets task (Пт, сен 15, 2023 5:21:40 )

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  // bracketsComfig.flat(), из str сделать Set с помощью цикла for и метода set.add(). Затем сравнить str и Set
 }
