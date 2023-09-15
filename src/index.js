4cd903dcfcf5eef17fc77edab8bc5f8b9cc6ea0a
NadyaGus <ftotkexit@gmail.com>
2023-09-15 18:00:05 +0300

    feat: add 2 solutions (Пт, сен 15, 2023 6:00:04 )

--- src/index.js
+++ src/index.js
@@ -2,15 +2,20 @@ module.exports = function check(str, bracketsConfig) {
   // your solution
   // bracketsComfig.flat(), из str сделать Set с помощью цикла for и метода set.add(). Затем сравнить str и Set
   let flatBrakets = bracketsConfig.flat();
+  let strBrakets = flatBrakets.toString().replace(",", "");
   let set = new Set();
-  
+
   for (let i = 0; i < str.length; i++) {
     set.add(i);
   }
 
   if (set === flatBrakets) {
     return true;
+  } else if (str === strBrakets) {
+    return true;
+  } else if (set === strBrakets) {
+    return true;
   } else {
     return false;
   }
-}
+};
