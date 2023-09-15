38d3c8bb38e50126ea306d9a121e60281971467e
NadyaGus <ftotkexit@gmail.com>
2023-09-15 17:36:41 +0300

    feat: add test solution (Пт, сен 15, 2023 5:36:41 )

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
   // bracketsComfig.flat(), из str сделать Set с помощью цикла for и метода set.add(). Затем сравнить str и Set
+  let flatBrakets = bracketsConfig.flat();
+  let set = new Set();
+  
+  for (let i = 0; i < str.length; i++) {
+    set.add(i);
+  }
+
+  if (set === flatBrakets) {
+    return true;
+  } else {
+    return false;
+  }
 }
